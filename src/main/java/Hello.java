659801f74be946b9ff8c633e2c36b76ea4dfac65
Tt <1773440504@qq.com>
2020-09-26 22:32:51 +0800

    练习

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
         System.out.println("你好世界");
         System.out.println("good night");
         System.out.println("see you tomorrow");
+        System.out.println("巴啦啦能量");
+        System.out.println("芜湖拉湖");
+        System.out.println("黑魔变身");
     }
 }
