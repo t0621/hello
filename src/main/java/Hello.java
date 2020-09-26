2a5fa12fe73ed75951145e2b89bb9eefb0f98bd3
Baleni <wang_chang_song@163.com>
2020-09-26 22:25:53 +0800

    晚安

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你好世界");
+        System.out.println("good night");
+        System.out.println("see you tomorrow");
     }
 }
